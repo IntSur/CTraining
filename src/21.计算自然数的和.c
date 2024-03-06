7ecbae74c87598009cfde87699843ccde59d78f6
IntSur <1203394464@qq.com>
2024-03-06 16:41:10 +0800

    1)update demo21

--- src/21.计算自然数的和.c
+++ src/21.计算自然数的和.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
  
+int addNumber(int int1) {
+    if(int1 != 0) {
+        return int1 + addNumber(int1 - 1);
+    } else {
+        return int1;
+    }
+}
+
 int main()
 {
     //用递归的方式实现
-    int i = 0;
-
-    
-
+    int stopint;
+    printf("请输入一个终止数：");
+    scanf("%d", &stopint);
+    int result = addNumber(stopint);
+    printf("result = %d\n", result);
     return 0;
 }
